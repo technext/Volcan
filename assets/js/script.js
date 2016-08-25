8f02a4e244501d1dc60074887a2cb5b2ec38a786
mnkakon <mahmudunnobi11@gmail.com>
2016-08-25 17:07:03 +0600

    pop-up

--- assets/js/script.js
+++ assets/js/script.js
@@ -94,3 +94,34 @@ $(document).ready(function () {
 		});
 
 });
+
+
+
+$('#open-popup').magnificPopup({
+    items: [
+      {
+        src: 'images/2.jpg' ,
+        title: 'Peter & Paul fortress in SPB'
+      },
+      {
+        src: 'http://vimeo.com/123123',
+        type: 'iframe' // this overrides default type
+      },
+      {
+        src: $('<div class="white-popup">Dynamically created element</div>'), // Dynamically created element
+        type: 'inline'
+      },
+      {
+        src: '<div class="white-popup">Popup from HTML string</div>', // HTML string
+        type: 'inline'
+      },
+      {
+        src: '#my-popup', // CSS selector of an element on page that should be used as a popup
+        type: 'inline'
+      }
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});
